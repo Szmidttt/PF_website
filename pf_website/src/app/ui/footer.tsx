cba412d7176eb203a554744fd9b252060d21a7fe
szmidttt <m13szmidt@gmail.com>
2025-06-22 08:15:44 +0200

    fixes

--- pf_website/src/app/ui/footer.tsx
+++ pf_website/src/app/ui/footer.tsx
@@ -1,8 +1,8 @@
 import { MapPinIcon } from "@heroicons/react/24/outline";
-export default function Nav(){
+export default function Footer(){
     return (<>
-    <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <MapPinIcon/>
+    <div className="fixed bottom-0 bg-blue-50">
+        COS
     </div>
         
     </>);
